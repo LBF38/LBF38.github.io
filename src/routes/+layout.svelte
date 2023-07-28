3b7d281b9281e024d0dc40b480fb7bde805a4b09
LBF38 <contact.lbf38@gmail.com>
2023-07-28 20:27:28 +0200

    feat(layout): :lipstick: initial layout
    playing around w/ layout and customization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+	import Navigation from './Navigation.svelte';
+
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<Navigation />
+	</svelte:fragment>
+	<div class="container p-6">
+		<slot />
+	</div>
+</AppShell>
---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<nav class="flex flex-row p-4 content-center justify-center">
+	<img src="favicon.png" alt="logo" class="btn-icon-xl" />
+	<ul class="container flex flex-row justify-evenly">
+		<li>
+			<a
+				href="/"
+				class="btn variant-soft hover:variant-filled-primary"
+				data-sveltekit-preload-data="hover"
+			>
+				Home
+			</a>
+		</li>
+		<li>
+			<a
+				href="/some-page"
+				class="btn variant-soft hover:variant-filled-primary"
+				data-sveltekit-preload-data="hover"
+			>
+				Projects
+			</a>
+		</li>
+		<li>
+			<a
+				href="/"
+				class="btn variant-soft hover:variant-filled-primary"
+				data-sveltekit-preload-data="hover"
+			>
+				@me
+			</a>
+		</li>
+		<li>
+			<a
+				href="/"
+				class="btn variant-soft hover:variant-filled-primary"
+				data-sveltekit-preload-data="hover"
+			>
+				Contact
+			</a>
+		</li>
+	</ul>
+	<LightSwitch />
+</nav>
