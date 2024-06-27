e1c70f631aa91d7d65a13ed3db68c8452d88ceb1
LBF38 <contact.lbf38@gmail.com>
2024-06-27 15:33:53 +0200

    feat(config): :sparkles: add links config for kit routes
    add social media links such as linkedin, instagram and github

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -35,9 +35,13 @@ const ACTIONS = {
  * LINKS
  */
 const LINKS = {
-  "gh_profile": (params: { username: (string | number) }) => {
+  "gh_profile": (params?: { username?: (string) }) => {
+    params = params ?? {}
+    params.username = params.username ?? 'LBF38'; 
     return `https://github.com/${params.username}`
   },
+  "linkedin": `https://www.linkedin.com/in/mathis-urien`,
+  "instagram": `https://www.instagram.com/mathis.u38/`,
   "shadcn_svelte": `https://shadcn-svelte.com`,
   "staticforms": `https://staticforms.xyz/`
 }
@@ -145,6 +149,6 @@ export type KIT_ROUTES = {
   PAGES: { '/': never, '/404.html': never, '/about': never, '/contact': never, '/contact/success': never, '/projects': never }
   SERVERS: Record<string, never>
   ACTIONS: Record<string, never>
-  LINKS: { 'gh_profile': 'username', 'shadcn_svelte': never, 'staticforms': never }
+  LINKS: { 'gh_profile': 'username', 'linkedin': never, 'instagram': never, 'shadcn_svelte': never, 'staticforms': never }
   Params: { username: never }
 }
