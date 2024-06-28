44bc26df29162a12c99e14c9c92107701d4f9e54
LBF38 <contact.lbf38@gmail.com>
2024-06-28 18:04:38 +0200

    fix(ui): :construction: trying to fix the scrollbar issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
+	import { navigating, page, updated } from '$app/stores';
 	import { route } from '$lib/ROUTES';
 	import Footer from '$lib/components/Footer.svelte';
 	import Navigation from '$lib/components/Navigation.svelte';
@@ -11,62 +11,90 @@
 	import Icon from '@iconify/svelte';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { ModeWatcher } from 'mode-watcher';
-	import { onDestroy, onMount, tick } from 'svelte';
+	import { afterUpdate, onDestroy, onMount, tick } from 'svelte';
 	import { Toaster } from 'svelte-sonner';
 	import type { Unsubscriber } from 'svelte/store';
 	import { blur, fade } from 'svelte/transition';
 	import '../app.pcss';
 	import type { LayoutData } from './$types';
+	import { debounce } from 'lodash-es';
 
 	let visible = true;
 	export let data: LayoutData;
 
 	function adjustScrollbarVisibility() {
 		if (!browser) return;
-		const needsScrolling = document.body.scrollHeight > window.innerHeight;
+		const needsScrolling = document.body.clientHeight > window.innerHeight;
 		console.log(
 			'adjustScrollbarVisibility: scrolling ?',
 			needsScrolling,
 			'body.scrollHeight',
 			document.body.scrollHeight,
+			'body.clientHeight',
+			document.body.clientHeight,
 			'window.innerHeight',
-			window.innerHeight
+			window.innerHeight,
+			'current page: ',
+			$page.route.id
 		);
 		document.body.classList.toggle('hide-scrollbar', !needsScrolling);
 	}
-	let unsubscribe: Unsubscriber;
+	const debouncedAdjustScrollbarVisibility = debounce(adjustScrollbarVisibility, 100);
 
-	function setup() {
-		adjustScrollbarVisibility();
-		window.addEventListener('resize', adjustScrollbarVisibility);
+	// function setup() {
+	// 	adjustScrollbarVisibility();
+	// 	window.addEventListener('resize', debouncedAdjustScrollbarVisibility);
 
-		unsubscribe = page.subscribe(async () => {
-			console.log('page changed - subscribe');
-			await tick();
-			if (document.readyState === 'complete') {
-				// If the document is already loaded, set up immediately.
-				adjustScrollbarVisibility();
-			} else {
-				// Otherwise, wait for the load event before setting up.
-				window.addEventListener('load', adjustScrollbarVisibility, { once: true });
-			}
+	// 	unsubscribe = page.subscribe(async ({ route }) => {
+	// 		afterUpdate(adjustScrollbarVisibility);
+	// 		// await tick();
+	// 		// console.log('page changed - subscribe', route.id);
+	// 		// if (document.readyState === 'complete') {
+	// 		// 	// If the document is already loaded, set up immediately.
+	// 		// 	console.log('doc ready');
+	// 		// 	adjustScrollbarVisibility();
+	// 		// } else {
+	// 		// 	// Otherwise, wait for the load event before setting up.
+	// 		// 	console.log('doc not ready');
+	// 		// 	window.addEventListener('load', adjustScrollbarVisibility, { once: true });
+	// 		// }
+	// 	});
+	// }
+
+	// onMount(() => {
+	// 	if (!browser) return;
+	// 	setup();
+	// });
+
+	// onDestroy(() => {
+	// 	if (!browser) return;
+	// 	window.removeEventListener('resize', debouncedAdjustScrollbarVisibility);
+	// 	if (unsubscribe) unsubscribe();
+	// });
+
+	// $: data.pathname, debouncedAdjustScrollbarVisibility();
+	// page.subscribe(async () => {
+	// 	await tick();
+	// 	debouncedAdjustScrollbarVisibility();
+	// });
+	$: $page,
+		afterUpdate(() => {
+			debouncedAdjustScrollbarVisibility();
 		});
-	}
-
-	onMount(() => {
-		if (!browser) return;
-		setup();
-	});
-
-	onDestroy(() => {
-		if (!browser) return;
-		window.removeEventListener('resize', adjustScrollbarVisibility);
-		if (unsubscribe) unsubscribe();
-	});
-
-	// $: $page, adjustScrollbarVisibility(), console.log('page changed - reactive');
 </script>
 
+<!-- <svelte:window
+	on:fullscreenchange={debouncedAdjustScrollbarVisibility}
+	on:resize={debouncedAdjustScrollbarVisibility}
+	on:load={debouncedAdjustScrollbarVisibility}
+	on:change={debouncedAdjustScrollbarVisibility}
+/>
+<svelte:body
+	on:resize={debouncedAdjustScrollbarVisibility}
+	on:change={debouncedAdjustScrollbarVisibility}
+	on:load={debouncedAdjustScrollbarVisibility}
+	on:fullscreenchange={debouncedAdjustScrollbarVisibility}
+/> -->
 <ParaglideJS {i18n}>
 	<Toaster richColors />
 	<ModeWatcher />
