ce1beee99d66385ee5d48adf8791f9391452a7bc
LBF38 <contact.lbf38@gmail.com>
2024-07-07 17:20:01 +0200

    fix(ui): :lipstick: update about page UI
    fix text alignment between metadata and content
    update translation to professional experience

--- src/lib/components/cv-item.svelte
+++ src/lib/components/cv-item.svelte
@@ -4,6 +4,7 @@
 	import { languageTag } from '$paraglide/runtime';
 	import { DateFormatter } from '@internationalized/date';
 	import { onMount } from 'svelte';
+	import { P } from './markdown/meltui';
 	import { H2 } from './typography/individual';
 
 	export let cv_content: Content<CV>[];
@@ -31,13 +32,13 @@
 	<H2 class="md:col-span-2 md:col-start-2 lg:col-span-3 lg:col-start-2">{title}</H2>
 	{#each cv_content as item}
 		<article class="col-span-full my-4 inline-grid gap-8 md:grid-cols-3 lg:grid-cols-4">
-			<p class="col-span-1 inline-flex flex-col md:text-end">
+			<P class="col-span-1 inline-flex flex-col md:text-end">
 				<span class="font-light">
 					{format(new Date(item.metadata.from))} - {format(new Date(item.metadata.to))}
 				</span>
 				<span class="font-bold">{item.metadata.what}</span>
 				<span class="font-light italic">{item.metadata.where}</span>
-			</p>
+			</P>
 			<div class="md:col-span-2 lg:col-span-3">
 				<svelte:component this={item.content} />
 			</div>
